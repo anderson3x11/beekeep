e01e6eec5279f3fe09932d4d8da5ce7eb0822d36
Thomas <126467625+thomascrecy@users.noreply.github.com>
2025-05-31 16:40:20 +0200

    changed number formatting

--- docs/main.js
+++ docs/main.js
@@ -141,12 +141,7 @@ class Game {
   }
 
   formatNumber(num) {
-    if (num < 1000) return num;
-    const units = ['K', 'M', 'B', 'T'];
-    const order = Math.floor(Math.log10(num) / 3);
-    const unitName = units[order - 1];
-    const value = num / Math.pow(1000, order);
-    return value.toFixed(1) + unitName;
+    return num.toLocaleString('en-US');
   }
 }
 
